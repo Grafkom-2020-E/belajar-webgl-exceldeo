5ca134e61f06b3161aa89dd5e601be20f13388a7
exceldeo <52096462+exceldeo@users.noreply.github.com>
2020-10-09 08:29:09 +0700

    berhasil menampilkan titik hijau

--- main.js
+++ main.js
@@ -4,12 +4,15 @@ function main() {
 
     var vertexShaderSource = `
     void main() {
+        gl_PointSize = 100.0;
+        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+
     }
     `;
 
     var fragmentShaderSource = `
     void main() {
-        
+        gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
     }
     `;
 
@@ -39,4 +42,6 @@ function main() {
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINTS, 0, 1);
 }
