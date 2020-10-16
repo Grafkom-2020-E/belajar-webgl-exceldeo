a258795e6902cefae4364c2c4aa481bc02c6017a
exceldeo <52096462+exceldeo@users.noreply.github.com>
2020-10-16 08:51:49 +0700

    berhasil menggambar 2 segitiga

--- main.js
+++ main.js
@@ -8,10 +8,12 @@ function main() {
     // Titik C (0.5 , 0.5)
 
     var vertices = [
-        -0.5, -0.5,       // Titik A
-        0.5, -0.5,        // Titik B
-        0.5, 0.5,         // Titik C
-        -0.5, 0.5,         // Titik C
+        -0.5, -0.5, 1.0, 0.0, 0.0,      // Titik A, MERAH
+        0.5, -0.5, 0.0, 1.0, 0.0,       // Titik B, HIJAU
+        0.5, 0.5, 0.0, 0.0, 1.0,        // Titik C, BIRU
+        -0.5, -0.5, 1.0, 1.0, 1.0,      // Titik A, PUTIH
+        0.5, 0.5, 1.0, 1.0, 1.0,        // Titik C, PUTIH
+        -0.5, 0.5, 1.0, 1.0, 1.0        // Titik D, PUTIH
       ];
 
     var vertexBuffer = gl.createBuffer();
@@ -50,23 +52,34 @@ function main() {
     //  tentang pengambilan data verteks dari ARRAY_BUFFER
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     var aPositionLoc = gl.getAttribLocation(shaderProgram, "a_Position");
+    var aColorLoc = gl.getAttribLocation(shaderProgram, "a_Color");
     gl.vertexAttribPointer(
         aPositionLoc, 
         2, 
         gl.FLOAT, 
         false, 
-        0, 
+        5 * Float32Array.BYTES_PER_ELEMENT, 
         0);
-    gl.enableVertexAttribArray(aPositionLoc);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+      gl.vertexAttribPointer(
+        aColorLoc, 
+        3, 
+        gl.FLOAT, 
+        false, 
+        5 * Float32Array.BYTES_PER_ELEMENT, 
+        2 * Float32Array.BYTES_PER_ELEMENT);
+      gl.enableVertexAttribArray(aPositionLoc);
+      gl.enableVertexAttribArray(aColorLoc);
+      gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.viewport(100, 0, canvas.height, canvas.height);
 
     // variable pertama note
-    var primitive = gl.TRIANGLE_FAN;
+    var primitive = gl.TRIANGLES;
     var offset = 0;
-    var nVertex = 4;
+    var nVertex = 6;
     gl.drawArrays(primitive, offset, nVertex);
     // gl.drawArrays(gl.POINTS, 0, 1); 
 }
